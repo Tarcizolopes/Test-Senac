d2c12e2c5228a08a63c50fe8b654b27fad393875
Tarcizo <tarcizolsj@gmail.com>
2023-01-31 20:42:09 -0300

    adiciona novo atributo
    Foi adicionado um novo atributo na classe Produto para indicar o tipo

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
-quantidade = int(input('Informe a quantidade: '))
-print(quantidade)
-
 class Produto:
-    def __init__(self, nome, preço):
+    def __init__(self, nome, preço, tipo):
         self.nome = nome
         self.preço = preço
+        self.tipo = tipo
 
+
+
+
+
+
+quantidade = int(input('Informe a quantidade: '))
+print(quantidade)
 
