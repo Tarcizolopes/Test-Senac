9d52d71dfed46597d4e06047c51bdcd8c1edf954
Tarcizo <tarcizolsj@gmail.com>
2023-01-31 20:16:52 -0300

    adiciona classe produto

--- main.py
+++ main.py
@@ -1,2 +1,9 @@
 quantidade = int(input('Informe a quantidade: '))
 print(quantidade)
+
+class Produto:
+    def __init__(self, nome, preço):
+        self.nome = nome
+        self.preço = preço
+
+
